75328a347b2e32e483164fb0da5073db8dee8d39
Andreas Kaminski <andreaskaminski75@web.de>
2025-11-13 12:15:23 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,103 @@
+{
+  description = "Syseleven-exporter, fetch syseleven specific metrics";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      bin =
+        {
+          version ? "dev",
+          ...
+        }:
+        pkgs.buildGoModule {
+          inherit version;
+          pname = "syseleven-exporter";
+          src = ./.;
+          env = {
+            CGO_ENABLED = 0;
+          };
+
+          ldflags = [
+            "-s"
+            "-w"
+            "-X github.com/syseleven/syseleven-exporter/pkg/version.Version=${version}"
+          ];
+
+          vendorHash = "sha256-DJa/74kIQebJhGYI6f+QVw1E5vhRioeBZd1ERUc/tp8=";
+          doCheck = false;
+        };
+
+      oci =
+        {
+          name ? "syseleven-exporter",
+          tag ? "latest",
+          ...
+        }:
+        pkgs.dockerTools.buildLayeredImage {
+          inherit name tag;
+          contents = [
+            pkgs.cacert
+          ];
+          config.Entrypoint = [
+            "${
+              pkgs.callPackage bin {
+                version = if tag == "latest" then "dev" else pkgs.lib.removePrefix "v" tag;
+              }
+            }/bin/syselevenexporter"
+          ];
+        };
+    in
+    {
+      packages.${system} = {
+        default = pkgs.callPackage bin { };
+        bin = pkgs.callPackage bin { };
+        oci = pkgs.callPackage oci { };
+      };
+
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          shellHook = ''
+            echo "nixpkgs revision: ${nixpkgs.rev}"
+            echo "    git revision: ${self.rev or self.dirtyRev or "dirty"}"
+          '';
+          buildInputs = with pkgs; [
+            go
+            gopls
+            gotools
+            go-tools
+            golangci-lint
+            goreleaser
+            go-junit-report
+            gocover-cobertura
+            curl
+            kind
+            kubectl
+            docker
+            trivy
+            nixfmt-rfc-style
+          ];
+        };
+      };
+      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
+    };
+}
